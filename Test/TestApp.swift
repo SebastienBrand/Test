43a7c4f701c142d186e15f1801832c050373a853
SebastienB <brandjsebastien@gmail.com>
2023-03-24 16:57:47 -0400

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Kelly Reardon-Sleicher on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
